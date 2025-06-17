019666ba16eb4ff41b9e18edf9c43fe6f9417de1
EkramjemalH <ekramjemalh@gmail.com>
2025-06-17 22:55:42 +0300

    update a file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let fullOutput = "";
+
+for (const message of messages) {
+  fullOutput += message + "\n";
+}
+console.log(fullOutput);
