21378cc136f2582f075f40c8209f7cd8a8643cf5
EkramjemalH <ekramjemalh@gmail.com>
2025-06-17 22:57:43 +0300

    correct the outpust

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-let fullOutput = "";
+const fullOutput = messages.join("\n");
 
-for (const message of messages) {
-  fullOutput += message + "\n";
-}
 console.log(fullOutput);
