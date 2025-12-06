25ee21fdb2f47708b21d62fd067c993f6a632749
EkramJemalH <ekramjemalh@gmail.com>
2025-12-06 09:13:33 +0300

    Update Note.txt and script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-
-let myfun=(a,b)=>a+b;
-console.log(myfun(2,6));
+class Reactangle{
+    constructor(width,height){
+        this.width=width;
+        this.height=height;
+    }
+    set width(newWidth){
+        if(newWidth>0){
+            this._width=newWidth;
+        }
+        else{
+            console.log("invalid width");
+        }
+    }
+    set height(newHeight){
+        if(newHeight>0){
+            this._height=newHeight;
+        }
+        else{
+            console.log("invalid Height");
+        }
+    }
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
+}
+const react=new Reactangle(-10,20);
+console.log(react.width);
+console.log(react.height);
